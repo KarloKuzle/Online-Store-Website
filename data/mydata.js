b04c7b9d6db41433c3372c69fbd5d2b0a7e6b378
Karlo Kuzle <113372288+KarloKuzle@users.noreply.github.com>
2024-05-11 19:35:34 +0200

    Add files via upload

---
+++ data/mydata.js
@@ -0,0 +1,275 @@
+module.exports = {
+    "website":"MYKEA",
+    "categories":[
+        {
+            "name":"Stolice",
+            "image":"../images/main_stolice.jpg",
+            "products":[
+                {
+                    "name":"Stolac1", "image":"../images/stolac1.png"
+                },
+                {
+                    "name":"Stolac2", "image":"../images/stolac2.jpg"
+                },
+                {
+                    "name":"Stolac3", "image":"../images/stolac3.jpg"
+                },
+                {
+                    "name":"Stolac4", "image":"../images/stolac4.jpg"
+                },
+                {
+                    "name":"Stolac5", "image":"../images/stolac5.jpg"
+                },
+                {
+                    "name":"Stolac6", "image":"../images/stolac6.jpg"
+                },
+                {
+                    "name":"Stolac7", "image":"../images/stolac7.jpg"
+                }
+            ]
+        },
+        {
+            "name": "Kreveti",
+            "image": "../images/main_kreveti.jpg",
+            "products":[
+                {
+                    "name":"Krevet1", "image":"../images/krevet1.jpg"
+                },
+                {
+                    "name":"Krevet2", "image":"../images/krevet2.jpg"
+                },
+                {
+                    "name":"Krevet3", "image":"../images/krevet3.jpg"
+                },
+                {
+                    "name":"Krevet4", "image":"../images/krevet4.jpg"
+                },
+                {
+                    "name":"Krevet5", "image":"../images/krevet5.jpg"
+                },
+                {
+                    "name":"Krevet6", "image":"../images/krevet6.jpg"
+                }
+            ]
+        },
+        {
+            "name": "Satovi",
+            "image": "../images/main_satovi.jpg",
+            "products": [
+                {
+                    "name":"Sat1", "image":"../images/sat1.jpg"
+                },
+                {
+                    "name":"Sat2", "image":"../images/sat2.jpg"
+                },
+                {
+                    "name":"Sat3", "image":"../images/sat3.jpg"
+                },
+                {
+                    "name":"Sat4", "image":"../images/sat4.jpg"
+                },
+                {
+                    "name":"Sat5", "image":"../images/sat5.jpg"
+                },
+                {
+                    "name":"Sat6", "image":"../images/sat6.jpg"
+                },
+                {
+                    "name":"Sat7", "image":"../images/sat7.jpg"
+                },
+                {
+                    "name":"Sat8", "image":"../images/sat8.jpg"
+                }
+            ]
+        },
+        {
+            "name": "Ormari",
+            "image": "../images/main_ormari.jpg",
+            "products": [
+                {
+                    "name":"Ormar1", "image":"../images/ormar1.jpg"
+                },
+                {
+                    "name":"Ormar2", "image":"../images/ormar2.jpg"
+                },
+                {
+                    "name":"Ormar3", "image":"../images/ormar3.jpg"
+                },
+                {
+                    "name":"Ormar4", "image":"../images/ormar4.jpg"
+                },
+                {
+                    "name":"Ormar5", "image":"../images/ormar5.jpg"
+                },
+                {
+                    "name":"Ormar6", "image":"../images/ormar6.jpg"
+                }
+            ]
+        },
+        {
+            "name": "Okviri",
+            "image": "../images/main_okviri.jpg",
+            "products": [
+                {
+                    "name":"Okvir1", "image":"../images/okvir1.jpg"
+                },
+                {
+                    "name":"Okvir2", "image":"../images/okvir2.jpg"
+                },
+                {
+                    "name":"Okvir3", "image":"../images/okvir3.jpg"
+                },
+                {
+                    "name":"Okvir4", "image":"../images/okvir4.jpg"
+                },
+                {
+                    "name":"Okvir5", "image":"../images/okvir5.jpg"
+                },
+                {
+                    "name":"Okvir6", "image":"../images/okvir6.jpg"
+                }
+            ]
+        },
+        {
+            "name": "Lampe",
+            "image": "../images/main_lampe.jpg",
+            "products": [
+                {
+                    "name":"Lampa1", "image":"../images/lampa1.jpg"
+                },
+                {
+                    "name":"Lampa2", "image":"../images/lampa2.jpg"
+                },
+                {
+                    "name":"Lampa3", "image":"../images/lampa3.jpg"
+                },
+                {
+                    "name":"Lampa4", "image":"../images/lampa4.jpg"
+                },
+                {
+                    "name":"Lampa5", "image":"../images/lampa5.jpg"
+                },
+                {
+                    "name":"Lampa6", "image":"../images/lampa6.jpg"
+                },
+                {
+                    "name":"Lampa7", "image":"../images/lampa7.jpg"
+                },
+                {
+                    "name":"Lampa8", "image":"../images/lampa8.jpg"
+                }
+            ]
+        },
+        {
+            "name": "Ogledala",
+            "image": "../images/main_ogledala.jpg",
+            "products": [
+                {
+                    "name":"Ogledalo1", "image":"../images/ogledalo1.jpg"
+                },
+                {
+                    "name":"Ogledalo2", "image":"../images/ogledalo2.jpg"
+                },
+                {
+                    "name":"Ogledalo3", "image":"../images/ogledalo3.jpg"
+                },
+                {
+                    "name":"Ogledalo4", "image":"../images/ogledalo4.jpg"
+                },
+                {
+                    "name":"Ogledalo5", "image":"../images/ogledalo5.jpg"
+                },
+                {
+                    "name":"Ogledalo6", "image":"../images/ogledalo6.jpg"
+                },
+                {
+                    "name":"Ogledalo7", "image":"../images/ogledalo7.jpg"
+                }
+            ]
+        },
+        {
+            "name": "Police",
+            "image": "../images/main_police.jpg",
+            "products": [
+                {
+                    "name":"Polica1", "image":"../images/polica1.jpg"
+                },
+                {
+                    "name":"Polica2", "image":"../images/polica2.jpg"
+                },
+                {
+                    "name":"Polica3", "image":"../images/polica3.jpg"
+                },
+                {
+                    "name":"Polica4", "image":"../images/polica4.jpg"
+                },
+                {
+                    "name":"Polica5", "image":"../images/polica5.jpg"
+                },
+                {
+                    "name":"Polica6", "image":"../images/polica6.jpg"
+                },
+                {
+                    "name":"Polica7", "image":"../images/polica7.jpg"
+                }
+            ]
+        },
+        {
+            "name": "Sofe",
+            "image": "../images/main_sofe.jpg",
+            "products": [
+                {
+                    "name":"Sofa1", "image":"../images/sofa1.jpg"
+                },
+                {
+                    "name":"Sofa2", "image":"../images/sofa2.png"
+                },
+                {
+                    "name":"Sofa3", "image":"../images/sofa3.jpg"
+                },
+                {
+                    "name":"Sofa4", "image":"../images/sofa4.jpg"
+                },
+                {
+                    "name":"Sofa5", "image":"../images/sofa5.jpg"
+                },
+                {
+                    "name":"Sofa6", "image":"../images/sofa6.jpg"
+                },
+                {
+                    "name":"Sofa7", "image":"../images/sofa7.jpg"
+                },
+                {
+                    "name":"Sofa8", "image":"../images/sofa8.jpg"
+                }
+            ] 
+        },
+        {
+            "name": "Stolovi",
+            "image": "../images/main_stolovi.jpg",
+            "products": [
+                {
+                    "name":"Stol1", "image":"../images/stol1.jpg"
+                },
+                {
+                    "name":"Stol2", "image":"../images/stol2.jpg"
+                },
+                {
+                    "name":"Stol3", "image":"../images/stol3.jpg"
+                },
+                {
+                    "name":"Stol4", "image":"../images/stol4.jpg"
+                },
+                {
+                    "name":"Stol5", "image":"../images/stol5.jpg"
+                },
+                {
+                    "name":"Stol6", "image":"../images/stol6.jpg"
+                },
+                {
+                    "name":"Stol7", "image":"../images/stol7.jpg"
+                }
+            ]
+        }
+    ]
+}
